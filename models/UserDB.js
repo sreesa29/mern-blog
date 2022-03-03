b45403ce560d914a97a67037357a5606f226b1e8
sreesa29 <98261473+sreesa29@users.noreply.github.com>
2022-03-03 11:59:29 +0530

    Update UserDB.js

--- models/UserDB.js
+++ models/UserDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://sreesankar:Krishna@cluster0.nshve.mongodb.net/ict-blog-bootcamp?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://sreesankar:krishna12@cluster0.nshve.mongodb.net/ict-blog-bootcamp?retryWrites=true&w=majority")
 const Schema = mongoose.Schema;
 
 var userSchema = new Schema({
